2bf2280949d5c089e433a2728bf3c3033381e846
sslmyo24 <39264300+sslmyo24@users.noreply.github.com>
2019-09-16 15:58:24 +0900

    B Module

--- 충남/B Module/js/apps.js
+++ 충남/B Module/js/apps.js
@@ -1,7 +1,8 @@
 const Model = new class {
 	init () {
 		this.db = openDatabase("Builder", "1.0", "teaser builder", 2*1024*1024)
-		const sql = `CREATE TABLE IF NOT EXISTS pagelist (code primary key, name, title, description, keyword)`
+		// const sql = `DROP TABLE pagelist`
+		const sql = `CREATE TABLE IF NOT EXISTS pagelist (idx integer primary key, code, name, title, description, keyword)`
 		return new Promise(async res => {
 			Model.query(sql)
 			res()
@@ -13,28 +14,59 @@ const Model = new class {
 			this.db.transaction(tx => { tx.executeSql(sql, arr, (tx, res) => { reso(res) }, (tx,error) => { console.log(error) }) })
 		})
 	}
+
+	async fetch (column, table, option = {arr:[], condition: false}) {
+		const sql = `SELECT ${column} FROM ${table}`
+		if (option.condition !== false) sql += option.condition
+		const list = await Model.query(sql, option.arr)
+		return list.rows
+	}
+
+	async rowCount (table, condition) {
+		const list = await Model.fetch("*", table, {arr:[], condition: condition})
+		return list.length
+	}
 }
 
 const Page = new class {
 
 	get pagelist () {
-		return Model.query(`SELECT * FROM pagelist`)
+		return Model.fetch("*", "pagelist")
 	}
 
 	async setList () {
+		const target = $("#page-list > ul")
+		$("li", target).remove()
 		const pagelist = await Page.pagelist
+		for (const v of pagelist) target.append(`<li><span class="name">${v.name}</span><button type="button" class="page-update modal-btn" data-idx="${v.idx}" data-target="update">페이지수정</button></li>`)
 	}
 
-	pageInsert (e) {
-		$("#page-list > ul").append(`<li><span class="name"><input type="text" name="name" id="new-page-name" /></span></li>`)
-		if ($("#new-page-name")) {
-			$("#new-page-name").keywdown(async function (e) {
-			if (e.keyCode === 13) {
-					await Model.query(`INSERT INTO pagelist (name) values(?)`, [e.target.value])
-					this.setList()
-				}
-			})
+	async newPage (e) {
+		if (e.keyCode === 13) {
+			await Model.query(`INSERT INTO pagelist (name) values(?)`, [e.target.value])
+			Page.setList()
 		}
+	}
+
+	async infoUpdate (e) {
+		e.preventDefault()
+		const idx = e.target.idx.value
+		const code = e.target.code.value
+		const name = e.target.name.value
+		const title = e.target.title.value
+		const description = e.target.description.value
+		const keyword = e.target.keyword.value
+
+		const reg = new RegExp(/^[0-9]+$/)
+		if (reg.test(~~code) === false) {
+			alert('고유코드는 숫자만 가능합니다.')
+			return false
+		}
+
+		await Model.query(`UPDATE pagelist SET code = ?, name = ?, title = ?, description = ?, keyword = ? where idx = ?`, [code, name, title, description, keyword, idx])
+		e.target.reset()
+		modalClose(`update`)
+		Page.setList()
 	}
 }
 
@@ -43,9 +75,14 @@ const loadOn = _ => {
 	.then(Page.setList)
 }
 
-const modalOpen = function () {
+const modalOpen = async function () {
 	const target = this.dataset.target
 	$(`#${target}`).css({'display':'flex'}).animate({opacity: 1}, 500)
+	if (target === 'update') {
+		const pagelist = await Model.fetch("*", "pagelist")
+		$(`#update [name="idx"]`).val(this.dataset.idx)
+		$(`#update [name="name"]`).val(pagelist[(~~this.dataset.idx) - 1].name)
+	}
 }
 
 const modalClose = function (id) {
@@ -56,6 +93,7 @@ const modalClose = function (id) {
 $(loadOn)
 .on("click", ".modal-btn", modalOpen)
 .on("click", ".modal-close", modalClose)
-.on("click", "#page-insert", Page.pageInsert)
-.on("submit", "#update form", Page.pageUpdate)
+.on("click", "#page-insert", _ => $("#page-list > ul").append(`<li><span class="name"><input type="text" name="name" id="new-page-name" /></span></li>`))
+.on("keydown", "#new-page-name", Page.newPage)
+.on("submit", "#update form", Page.infoUpdate)
 
