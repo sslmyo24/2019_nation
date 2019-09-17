f995668862928e5cad3b5f229bfcbdd255337b14
sslmyo24 <39264300+sslmyo24@users.noreply.github.com>
2019-09-17 11:33:15 +0900

    B Module

--- 충남/B Module/js/apps.js
+++ 충남/B Module/js/apps.js
@@ -30,45 +30,28 @@ const Model = new class {
 	}
 }
 
-let selected = null
-const Page = new class {
+let selectIdx = null, selectType = null
+const Page = class {
+
+	constructor () {
+		this.getHTML('./Header.HTML').then(doc => new Promise(res => res(`<div class="template" data-type="header">${doc}</div>`)))
+		.then(text => this.getHTML('./Footer.HTML').then(doc => new Promise(res => res(text + `<div class="template" data-type="footer">${doc}</div>`))))
+		.then(text => this.default = text)
+	}
 
 	get pagelist () {
 		return Model.fetch("*", "pagelist")
 	}
 
+	get defaultLayout () {
+		return this.default
+	}
+
 	async setList (list = false) {
 		const target = $("#page-list > ul")
 		$("li", target).remove()
-		if (list === false) list = await Page.pagelist
+		if (list === false) list = await this.pagelist
 		for (const v of list) target.append(`<li data-idx="${v.idx}"><span class="name">${v.name}</span><button type="button" class="page-update modal-btn" data-target="update">페이지수정</button></li>`)
-	}
-
-	async newPage (e) {
-		if (e.keyCode === 13) {
-			await Model.query(`INSERT INTO pagelist (name) values(?)`, [e.target.value])
-			Page.setList()
-			const lastIdx = await Model.rowCount(`pagelist`)
-			let text = ``
-			await Page.getHTML('./Header.HTML').then(doc => text += `<div class="template" data-type="header">${doc}</div>`)
-			await Page.getHTML('./Footer.HTML').then(doc => text += `<div class="template" data-type="footer">${doc}</div>`)
-			Model.query(`INSERT INTO layout (pidx, html) values(?, ?)`, [lastIdx, text])
-		}
-	}
-
-	infoUpdate (e) {
-		e.preventDefault()
-
-		const reg = new RegExp(/^[0-9]+$/)
-		if (reg.test(~~e.target.code.value) === false) {
-			alert('고유코드는 숫자만 가능합니다.')
-			return false
-		}
-
-		Model.query(`UPDATE pagelist SET code = ?, name = ?, title = ?, description = ?, keyword = ? where idx = ?`, [...(new FormData(e.target)).values()])
-		modalClose(`update`)
-		e.target.reset()
-		Page.setList()
 	}
 
 	async getHTML (url) {
@@ -77,33 +60,23 @@ const Page = new class {
 			return parser.parseFromString(html, 'text/html')
 		})
 		return new Promise(res => {
-			res($(doc.documentElement).find('body')[0].innerHTML)
+			res($(doc.documentElement).find('body').html())
 		})
 	}
 
-	async preview () {
-		if ($(this).hasClass('active')) return
-		$('#page-list .active').removeClass('active')
-		$(this).addClass('active')
-		selected = ~~this.dataset.idx
-		const target = $("#preview > div")
-		$("*", target).remove()
-		const html = await Model.fetch("html", "layout", {arr: [selected], condition: " where pidx = ?"} )
-		target.append(html[0].html)
-	}
-
 	updateHTML () {
-		const html = $("#preview > div")[0].innerHTML
-		Model.query(`UPDATE layout SET html = ? where pidx = ?`, [html, selected])
+		const html = $("#preview > div").html()
+		Model.query(`UPDATE layout SET html = ? where pidx = ?`, [html.replace('template active', 'template'), selectIdx])
 	}
 }
 
+const page = new Page()
 const loadOn = _ => {
 	Model.init()
-	.then(Page.setList)
+	.then(page.setList)
 }
 
-const modalOpen = async function () {
+const modalOpen = async function (e) {
 	const target = this.dataset.target
 	$(`#${target}`).css({'display':'flex'}).animate({opacity: 1}, 500)
 	if (target === 'update') {
@@ -124,18 +97,140 @@ const modalClose = function (id) {
 	target.fadeOut(300, function () { $(this).css({'opacity': '0'}) })
 }
 
+const addPage = e => {
+	if (e.keyCode === 13) {
+		Model.query(`INSERT INTO pagelist (name) values(?)`, [e.target.value])
+		.then(_ => page.setList())
+		.then(async _ => {
+			const lastIdx = await Model.rowCount(`pagelist`)
+			Model.query(`INSERT INTO layout (pidx, html) values(?, ?)`, [lastIdx, page.default])
+		})
+		
+	}
+}
+
+const infoUpdate = e => {
+	e.preventDefault()
+
+	const reg = new RegExp(/^[0-9]+$/)
+	if (reg.test(~~e.target.code.value) === false) {
+		alert('고유코드는 숫자만 가능합니다.')
+		return false
+	}
+
+	Model.query(`UPDATE pagelist SET code = ?, name = ?, title = ?, description = ?, keyword = ? where idx = ?`, [...(new FormData(e.target)).values()])
+	modalClose(`update`)
+	e.target.reset()
+	page.setList()
+}
+
+const removePage = _ => {
+	if (selectIdx === null) return
+	Model.query(`DELETE FROM pagelist where idx = ?`, [selectIdx])
+	Model.query(`DELETE FROM layout where pidx = ?`, [selectIdx])
+	.then(_ => page.setList())
+	$("#preview .template").remove()
+}
+
+const preview = async function () {
+	const parent = $(this).parent()
+	if (parent.hasClass('active')) return
+	$('#page-list .active').removeClass('active')
+	parent.addClass('active')
+	selectIdx = ~~parent[0].dataset.idx
+	const target = $("#preview > div")
+	$("*", target).remove()
+	const html = await Model.fetch("html", "layout", {arr: [selectIdx], condition: " where pidx = ?"} )
+	target.append(html[0].html)
+}
+
+const newTemplate = async function () {
+	const type = this.innerHTML
+	page.getHTML(`./${type}.HTML`.replace(`&amp;`, '&'))
+	.then(doc => $(`[data-type="footer"]`).before(`<div class="template" data-type="${type.substring(0, type.length - 2)}">${doc}</div>`))
+	.then(page.updateHTML)
+}
+
+const selectTemplate = function () {
+	$(".edit").hide()
+	if ($(this).hasClass('active')) selectType = null
+	else {
+		selectType = this.dataset.type
+		$('.template.active').removeClass('active')
+	}
+	$(this).toggleClass('active')
+}
+
+const layoutReset = function () {
+	if (selectIdx === null) return
+	$("#preview > div").html(page.default)
+	page.updateHTML()
+}
+
+const startSetting = _ => {
+	if (selectType === null) return
+	$(`.edit`).hide()
+	$(`#${selectType}-edit`).show()
+	switch (selectType) {
+		case 'header':
+			const html = $("#gnb > ul").html() + `<li><input type="text" id="new-menu"></li>`
+			const reg = new RegExp('</a></li>', 'g')
+			$("#menu-list > ul").html(html.replace(reg, `</a><button type="btn" class="remove-btn">x</button></li>`))
+			if ($("#menu-list li").length >= 6) $("#menu-list li:last-child").remove()
+			break;
+	}
+}
+
+const logoChange = e => {
+	modalClose('logo')
+	const file = e.target.files[0]
+	const reader = new FileReader()
+	reader.readAsDataURL(file)
+	reader.onload = _ => {
+		e.target.value = null
+		$("#site-logo img").attr('src', reader.result)
+		page.updateHTML()
+	}
+}
+
+const addMenu = function (e) {
+	if (e.keyCode === 13) {
+		const len = $("#menu-list li:not(.input)").length, parent = $(this).parent()
+		if (len >= 5) {
+			alert('메뉴는 5개 이내여야 합니다.')
+			return false
+		}
+		parent.before(`<li><a href="#">${e.target.value}</a><button type="btn" class="remove-btn"></button></li>`)
+		$("#gnb > ul").append(`<li><a href="#">${e.target.value}</a></li>`)
+		if (len === 4) parent.remove()
+		page.updateHTML()
+	}
+}
+
+const removeMenu = function () {
+	const len = $("#menu-list li:not(.input)").length
+	if (len <= 3) {
+		alert('메뉴는 3개 이상이어야합니다.')
+		return false
+	}
+	if (len === 5) $(this).parents('ul').append(`<li><input type="text" id="new-menu"></li>`)
+	$(this).parent().remove()
+}
+
 $(loadOn)
 .on("click", ".modal-btn", modalOpen)
 .on("click", ".modal-close", modalClose)
 .on("click", "#page-insert", _ => $("#page-list ul").append(`<li class="new"><input type="text" name="name" id="new-page-name" value="신규 페이지" /></li>`))
-.on("keydown", "#new-page-name", Page.newPage)
-.on("submit", "#update form", Page.infoUpdate)
-.on("click", "#page-list li:not(.new)", Page.preview)
+.on("keydown", "#new-page-name", addPage)
+.on("submit", "#update form", infoUpdate)
+.on("click", "#page-delete", removePage)
+.on("click", "#page-list li:not(.new) > .name", preview)
 .on("click", "#layout-insert", _ => $("#template-list").fadeIn())
-.on("click", "#template-list li", function () {
-	const type = this.innerHTML
-	Page.getHTML(`./${type}.HTML`.replace(`&amp;`, '&'))
-	.then(doc => $("footer").before(`<div class="template" data-type="${type}">${doc}</div>`))
-	Page.updateHTML()
-})
+.on("click", "#template-list li", newTemplate)
+.on("click", "#preview .template", selectTemplate)
+.on("click", "#layout-reset", layoutReset)
+.on("click", "#layout-setting", startSetting)
+.on("change", "#logo input", logoChange)
+.on("keydown", "#new-menu", addMenu)
+.on("click", ".remove-btn", removeMenu)
 
